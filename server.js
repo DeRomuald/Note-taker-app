4c0739f7bccef94e8e04b6d33b19378363bfa123
DeRomuald <deromuald.romulus@gmail.com>
2022-08-31 23:15:54 -0400

    exported index files to router

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const apiRouter = require('./routes/apiRoutes');
+const htmlRouter = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
